226b1fbe7481bb9add45e64dcb8ca3a5f49271d5
Uriah Pissalidis <44508430+uriahpissalidis@users.noreply.github.com>
2022-07-17 20:03:10 -0400

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,21 +1,24 @@
-const express = require("express");
-const colors = require("colors");
-require("dotenv").config();
-const { graphqlHTTP } = require("express-graphql");
-const schema = require("./schema/schema");
-const connectDB = require(".config/db");
-const port = process.env.PORT || 5000; //read from env file
+const express = require('express');
+const colors = require('colors');
+const cors = require('cors');
+require('dotenv').config();
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+const connectDB = require('./config/db');
+const port = process.env.PORT || 5000;
 
 const app = express();
 
-//connect to database
+// Connect to database
 connectDB();
 
+app.use(cors());
+
 app.use(
-  "/graphql",
+  '/graphql',
   graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === "development",
+    graphiql: process.env.NODE_ENV === 'development',
   })
 );
 
